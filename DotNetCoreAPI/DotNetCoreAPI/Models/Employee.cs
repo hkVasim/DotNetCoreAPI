125290cbf2c7c9a799222ee5a48a06d76e3a6ad9
hkVasim <ht.wasim@gmail.com>
2020-05-03 14:17:44 +0530

    Basic Changes

---
+++ DotNetCoreAPI/DotNetCoreAPI/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreAPI.Models
+{
+    public class Employee
+    {
+        /// <summary>
+        /// Gets or Sets EmployeeID
+        /// </summary>
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// Gets or Sets FirstName
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or Sets LastName
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Age
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
